941ce334b568d09344fa8813a4330765e8e7fd40
Sparkworm <imnota.squirp@gmail.com>
2024-09-03 13:04:51 -0500

    Added a custom indent singleton to manage continuous indentatio accross scopes.

---
+++ general/custom_indent.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var indent_count: int = 0
+
+func increase_indent(amnt:=1) -> void:
+	indent_count += amnt
+
+func decrease_indent(amnt:=1) -> void:
+	indent_count -= amnt
+	if indent_count < 0:
+		print_rich("[color=red][b]INDENT COUNT LESS THAN 0![/b][/color]")
+
+func set_indent(amnt) -> void:
+	indent_count = amnt
+	if indent_count < 0:
+		print_rich("[color=red][b]INDENT COUNT LESS THAN 0![/b][/color]")
+
+func _to_string() -> String:
+	var str: String = ""
+	for i in range(1,indent_count):
+		str += "\t"
+	return str
